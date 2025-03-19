d1268c4d952f6267665e1193640d7974af2f1c39
AlexandreDresch <connectWithAlexandre@proton.me>
2025-03-18 23:46:05 -0300

    feat: add FetchWrapper to data provider

--- src/providers/data-provider/index.ts
+++ src/providers/data-provider/index.ts
@@ -5,12 +5,39 @@ import dataProviderNestjsQuery, {
   liveProvider as liveProviderNestjsQuery,
 } from "@refinedev/nestjs-query";
 import { createClient } from "graphql-ws";
+import { fetchWrapper } from "./fetch-wrapper";
 
 const API_URL = "https://api.nestjs-query.refine.dev/graphql";
 const WS_URL = "wss://api.nestjs-query.refine.dev/graphql";
+const API_BASE_URL = "https://api.crm.refine.dev";
 
-const gqlClient = new GraphQLClient(API_URL);
-const wsClient = createClient({ url: WS_URL });
+const gqlClient = new GraphQLClient(API_URL, {
+  fetch: (url: string, options: RequestInit) => {
+    try {
+      return fetchWrapper(url, options);
+    } catch (error) {
+      return Promise.reject(error as Error);
+    }
+  },
+});
+
+const wsClient =
+  typeof window !== undefined
+    ? createClient({
+        url: WS_URL,
+        connectionParams: () => {
+          const accessToken = window.localStorage.getItem("access_token");
+
+          return {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          };
+        },
+      })
+    : undefined;
 
 export const dataProvider = dataProviderNestjsQuery(gqlClient);
-export const liveProvider = liveProviderNestjsQuery(wsClient);
+export const liveProvider = wsClient
+  ? liveProviderNestjsQuery(wsClient)
+  : undefined;
