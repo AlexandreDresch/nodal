ababe7f9efa99b669c129f8722b5b98c99bb210d
AlexandreDresch <connectWithAlexandre@proton.me>
2025-05-01 23:07:34 -0300

    feat: add base features component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
+import Features from "@/components/landing-page/features";
 import Hero from "@/components/landing-page/hero";
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col">
       <Hero />
+
+      <Features />
     </div>
   );
 }
