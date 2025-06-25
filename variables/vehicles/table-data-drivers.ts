6053f25be28a193783efcb9ddde91d5bebd1bcd4
AlexandreDresch <connectWithAlexandre@proton.me>
2025-06-24 23:00:36 -0300

    feat: add DriversTable component and sample driver data

---
+++ variables/vehicles/table-data-drivers.ts
@@ -0,0 +1,58 @@
+type RowObj = {
+  name: string[];
+  deliveries: number;
+};
+
+const tableColumnsDrivers: RowObj[] = [
+  {
+    name: [
+      "Jose Gonzalez",
+      "/images/user.png",
+    ],
+    deliveries: 9821,
+  },
+  {
+    name: [
+      "Maria Rodriguez",
+      "/images/user.png",
+    ],
+    deliveries: 7032,
+  },
+  {
+    name: [
+      "Liam Smith",
+      "/images/user.png",
+    ],
+    deliveries: 5204,
+  },
+  {
+    name: [
+      "Emma Johnson",
+      "/images/user.png",
+    ],
+    deliveries: 4309,
+  },
+  {
+    name: [
+      "Noah Williams",
+      "/images/user.png",
+    ],
+    deliveries: 3871,
+  },
+  {
+    name: [
+      "Olivia Brown",
+      "/images/user.png",
+    ],
+    deliveries: 3152,
+  },
+  {
+    name: [
+      "Elijah Davis",
+      "/images/user.png",
+    ],
+    deliveries: 2907,
+  },
+];
+
+export default tableColumnsDrivers;
